66e342305d533e686062b47ef8c07a5780351f95
jounaidr <jounaidruhomaun@googlemail.com>
2026-09-04 16:01:23 +0100

    use log config in slurmstack.py

--- slurmstack.py
+++ slurmstack.py
@@ -3,11 +3,11 @@
 slurmstack
 """
 import logging
+import logging.config
 
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-
 
     logger.info("================")
     logger.info("Initializing VMs")
