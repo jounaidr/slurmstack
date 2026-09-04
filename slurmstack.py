2036e81a304f52b5ed8153eaa2cb00dfae0ddb7b
jounaidr <jounaidruhomaun@googlemail.com>
2026-09-04 12:08:53 +0100

    add main script

---
+++ slurmstack.py
@@ -0,0 +1,14 @@
+#!/bin/python3.11
+"""
+slurmstack
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+
+
+    logger.info("================")
+    logger.info("Initializing VMs")
+    logger.info("================")
